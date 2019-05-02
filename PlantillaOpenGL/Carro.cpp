44a36b11caca4212ad345d0a74228c8cfcd463e9
Javier Eliud Lizarraga Martin Del Campo <jelmdc@hotmail.com>
2019-05-02 09:48:20 -0700

    Se mueve

--- PlantillaOpenGL/Carro.cpp
+++ PlantillaOpenGL/Carro.cpp
@@ -2,6 +2,18 @@
 #include "stdafx.h"
 #include "Carro.h"
 
+void Carro::actualizarMatrizModelo() {
+	//Matriz identidad
+	modelo = mat4(1.0f);
+	modelo = translate(modelo, coordenadas);
+
+}
+
+void Carro::avanzar() {
+	coordenadas.z += 0.01;
+	actualizarMatrizModelo();
+}
+
 Carro::Carro() {
 	vertices.push_back({ vec4(-0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
 	vertices.push_back({ vec4(0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
@@ -30,10 +42,10 @@ Carro::Carro() {
 	vertices.push_back({ vec4(0.5f, -0.05f,  2.0f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
 
 
-	/*cuadrado->vertices.push_back({ vec4(-0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
-	cuadrado->vertices.push_back({ vec4(0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
-	cuadrado->vertices.push_back({ vec4(0.5f, 0.40f, 0.5f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
-	cuadrado->vertices.push_back({ vec4(-0.5f, 0.40f, 0.5f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });*/
+	/*vertices->vertices.push_back({ vec4(-0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
+	vertices->vertices.push_back({ vec4(0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
+	vertices->vertices.push_back({ vec4(0.5f, 0.40f, 0.5f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
+	vertices->vertices.push_back({ vec4(-0.5f, 0.40f, 0.5f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });*/
 
 
 	vertices.push_back({ vec4(-0.5f, 0.40f, 0.5f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
